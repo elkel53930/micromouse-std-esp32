0f5fabfbe23012fcd796254ac8ffdcac52a290e7
Kazuki Iida <elkel53930@gmail.com>
2023-08-21 08:40:20 +0900

    initial commit
    Signed-off-by: Kazuki Iida <elkel53930@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use log::*;
+
+fn main() -> anyhow::Result<()> {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_sys::link_patches();
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = PinDriver::output(peripherals.pins.gpio20)?;
+
+    info!("Welcome to ExtraICE4");
+
+    loop {
+        led.set_high()?;
+        FreeRtos::delay_ms(1000);
+        led.set_low()?;
+        FreeRtos::delay_ms(1000);
+    }
+}
