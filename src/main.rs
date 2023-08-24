e54aa23b23790e51299cdcf839ec85245c1ca289
Kazuki Iida <elkel53930@gmail.com>
2023-08-24 21:00:28 +0900

    100us periodic interrupt
    Signed-off-by: Kazuki Iida <elkel53930@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use esp_idf_hal::adc::{config::Config, AdcChannelDriver, AdcDriver, Atten11dB};
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripheral::Peripheral;
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::timer;
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_idf_sys::{
     mcpwm_config_t, mcpwm_counter_type_t_MCPWM_UP_COUNTER, mcpwm_duty_type_t_MCPWM_DUTY_MODE_0,
     mcpwm_generator_t_MCPWM_GEN_A, mcpwm_generator_t_MCPWM_GEN_B, mcpwm_gpio_init, mcpwm_init,
-    mcpwm_io_signals_t_MCPWM0A, mcpwm_io_signals_t_MCPWM0B, mcpwm_set_duty, mcpwm_set_pin,
+    mcpwm_io_signals_t_MCPWM0A, mcpwm_io_signals_t_MCPWM0B, mcpwm_set_duty,
     mcpwm_timer_t_MCPWM_TIMER_0, mcpwm_unit_t_MCPWM_UNIT_0,
 };
+
+static mut LED_GREEN: Option<PinDriver<'_, Gpio19, Output>> = None;
+static mut LED_BLUE: Option<PinDriver<'_, Gpio20, Output>> = None;
+static mut LED_RED: Option<PinDriver<'_, Gpio21, Output>> = None;
 
 fn set_motor_speed(speed: f32) {
     if speed > 0.0 {
@@ -46,20 +52,32 @@ fn set_motor_speed(speed: f32) {
     }
 }
 
+fn init_led(peripherals: &mut Peripherals) -> anyhow::Result<()> {
+    unsafe {
+        LED_GREEN = Some(PinDriver::output(
+            peripherals.pins.gpio19.clone_unchecked(),
+        )?);
+        LED_BLUE = Some(PinDriver::output(
+            peripherals.pins.gpio20.clone_unchecked(),
+        )?);
+        LED_RED = Some(PinDriver::output(
+            peripherals.pins.gpio21.clone_unchecked(),
+        )?);
+        LED_GREEN.as_mut().unwrap().set_high()?;
+        LED_BLUE.as_mut().unwrap().set_high()?;
+        LED_RED.as_mut().unwrap().set_high()?;
+    }
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+
     esp_idf_sys::link_patches();
 
-    let peripherals = Peripherals::take().unwrap();
+    let mut peripherals = Peripherals::take().unwrap();
 
-    // Initialize the blue LED
-    let mut green = PinDriver::output(peripherals.pins.gpio19)?;
-    let mut blue = PinDriver::output(peripherals.pins.gpio20)?;
-    let mut red = PinDriver::output(peripherals.pins.gpio21)?;
-    //    let mut cwccw_r = PinDriver::output(peripherals.pins.gpio37)?; // CWCCW_R
-    //    let _ = PinDriver::output(peripherals.pins.gpio36)?; // PWM_R
-    let mut mot_sleep = PinDriver::output(peripherals.pins.gpio38)?;
+    // Initialize LEDs
+    init_led(&mut peripherals)?;
 
     unsafe {
         //        mcpwm_gpio_init(mcpwm_unit_t_MCPWM_UNIT_0, mcpwm_io_signals_t_MCPWM0A, 36);
@@ -93,37 +111,36 @@ fn main() -> anyhow::Result<()> {
         );
     }
 
+    let mut mot_sleep = PinDriver::output(peripherals.pins.gpio38)?;
     mot_sleep.set_high()?;
-
-    let mut speed = 0.0;
-    let mut diff = 4.0;
+    set_motor_speed(0.0);
 
     // Initialize the ADC
     let mut adc = AdcDriver::new(peripherals.adc1, &Config::new().calibration(true))?;
     let mut adc_pin: AdcChannelDriver<'_, Gpio5, Atten11dB<_>> =
         AdcChannelDriver::new(peripherals.pins.gpio5)?;
 
-    loop {
-        blue.set_low()?;
-        let adc_value = adc.read(&mut adc_pin).unwrap();
-        blue.set_high()?;
-        red.set_low()?;
-        println!("ADC value: {}", adc_value);
-        red.set_high()?;
-        green.set_low()?;
-        FreeRtos::delay_ms(50);
-        green.set_high()?;
-        FreeRtos::delay_ms(50);
+    let timer_config = timer::TimerConfig::new().auto_reload(true);
+    let mut timer = timer::TimerDriver::new(peripherals.timer00, &timer_config)?;
+    timer.set_alarm(100)?;
+    unsafe {
+        timer.subscribe(timer_isr)?;
+    }
 
-        speed = speed + diff;
-        if speed >= 50.0 {
-            speed = 50.0;
-            diff = -2.0;
-        } else if speed <= -50.0 {
-            speed = -50.0;
-            diff = 2.0;
-        }
-        println!("speed: {}", speed);
-        set_motor_speed(speed);
+    timer.enable_alarm(true)?;
+    timer.enable(true)?;
+
+    loop {
+        let adc_value = adc.read(&mut adc_pin).unwrap();
+        println!("ADC value: {}", adc_value);
+        FreeRtos::delay_ms(100);
+    }
+}
+
+fn timer_isr() {
+    unsafe {
+        LED_GREEN.as_mut().unwrap().toggle().unwrap();
+        LED_BLUE.as_mut().unwrap().toggle().unwrap();
+        LED_RED.as_mut().unwrap().toggle().unwrap();
     }
 }
